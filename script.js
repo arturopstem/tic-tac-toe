a2afa7d06e9676dac220c6d8e2979cdf3edbdcd8
arturop <arturopstem@gmail.com>
2023-05-04 17:44:03 -0500

    Add logic to draw the winning line

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const Board = (() => {
       cell.classList.add('empty');
       cell.replaceChildren();
     });
+    const winningLineBox = document.querySelector('.winningLineBox');
+    winningLineBox.innerHTML = '';
   }
 
   function getRandomEmptyCell() {
@@ -38,7 +40,56 @@ const Board = (() => {
     cell.classList.remove('empty');
     cell.classList.add(mark);
   }
-  return { init, drawMark, getRandomEmptyCell, hasEmptyCells };
+
+  function formatSVG(direction, color, distance, gridArea) {
+    const svg = document.createElement('svg');
+    svg.classList.add('winningMark');
+    svg.style.setProperty('--distance', distance);
+    svg.style.setProperty('--stroke', color);
+    svg.style.setProperty('grid-area', gridArea);
+    const use = document.createElement('use');
+    use.setAttribute('href', `#${direction}`);
+    svg.appendChild(use);
+    return svg.outerHTML;
+  }
+
+  function drawWinningLine(winner, winningLine) {
+    const smallLine = 121.6552506059644;
+    const longLine = 136.01470508735443;
+    const color = winner === 'cross' ? 'green' : 'red';
+    let svgLine;
+    switch (winningLine) {
+      case '012':
+        svgLine = formatSVG('horizontal__line', color, smallLine, '1/1/2/4');
+        break;
+      case '345':
+        svgLine = formatSVG('horizontal__line', color, smallLine, '2/1/3/4');
+        break;
+      case '678':
+        svgLine = formatSVG('horizontal__line', color, smallLine, '3/1/4/4');
+        break;
+      case '036':
+        svgLine = formatSVG('vertical__line', color, smallLine, '1/1/4/2');
+        break;
+      case '147':
+        svgLine = formatSVG('vertical__line', color, smallLine, '1/2/4/3');
+        break;
+      case '258':
+        svgLine = formatSVG('vertical__line', color, smallLine, '1/3/4/4');
+        break;
+      case '246':
+        svgLine = formatSVG('diagonal__right', color, longLine, '1/1/4/4');
+        break;
+      case '048':
+        svgLine = formatSVG('diagonal__left', color, longLine, '1/1/4/4');
+        break;
+      default:
+    }
+    const winningLineBox = document.querySelector('.winningLineBox');
+    winningLineBox.innerHTML = svgLine;
+  }
+
+  return { init, drawMark, getRandomEmptyCell, hasEmptyCells, drawWinningLine };
 })();
 
 const Game = (() => {
@@ -52,6 +103,7 @@ const Game = (() => {
     if (winner) {
       console.log(`${winner} wins with line ${winningLine}`);
       console.log('Game ended');
+      Board.drawWinningLine(winner, winningLine);
     }
   }
 
